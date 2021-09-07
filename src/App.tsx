35bf6a8a798785e990fa1022e63fcfa1dbde3665
zamFe <zamfel18@student.kristiania.no>
2021-09-07 23:17:34 +0200

    started working on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import {useState} from 'react';
+
+import { Component } from 'react';
+import searchIcon from './assets/magnifying-glass.svg';
 
 function App() {
+  const [title, setTitle] = useState('');
   return (
-    <p>
-      Edit <code>src/App.tsx</code> and save to reload.
-    </p>
+    <div className="search-flow-container">
+    <h3 className="flow-title">Search Flow</h3>
+        <form action="/form/submit" method="post">
+            <div className="input-container">
+                <input 
+                  id="focusedSearch" 
+                  className="search-input" 
+                  type="search"
+                  required
+                  placeholder="Search"
+                  onChange={(e) => setTitle(e.target.value)}
+                  />
+                <img className="search-icon" src={searchIcon}></img>
+            </div>
+        </form>
+</div>
   );
 }
 
