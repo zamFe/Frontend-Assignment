8af4e60a8ce701911aa0ba3c9d15e373d1aac82c
zamFe <felixza037@gmail.com>
2021-09-08 20:23:52 +0200

    set up search bar with cross-icon to clear

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,25 @@ import {useState} from 'react';
 
 import { Component } from 'react';
 import searchIcon from './assets/magnifying-glass.svg';
+import crossIcon from './assets/cross.svg';
 
 function App() {
-  const [title, setTitle] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const [crossIconVisible, setCrossIconVisible] = useState('visible');
+
+  function onSearchInputChange(input: string) {
+    setInputValue(input);
+    setCrossIconVisible((input.length > 0) ? 'visible' : 'hidden');
+  }
+
+  function clearInput() {
+    setInputValue('');
+  }
+
+  function runSearch(input : string) {
+    
+  }
+
   return (
     <div className="search-flow-container">
     <h3 className="flow-title">Search Flow</h3>
@@ -17,9 +33,21 @@ function App() {
                   type="search"
                   required
                   placeholder="Search"
-                  onChange={(e) => setTitle(e.target.value)}
+                  value={inputValue}
+                  onChange={(e) => onSearchInputChange(e.target.value)}
                   />
-                <img className="search-icon" src={searchIcon}></img>
+                  <div 
+                    className="cross-icon-container icon-container"
+                    onClick={(e) => clearInput()}>
+                    <img 
+                      className="cross-icon icon"  
+                      src={crossIcon}></img>
+                  </div>
+                    <div 
+                      className="search-icon-container icon-container"
+                      onClick={(e) => runSearch(inputValue)}>
+                      <img className="search-icon icon" src={searchIcon}></img>
+                  </div>
             </div>
         </form>
 </div>
